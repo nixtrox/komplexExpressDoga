baf4deb8f0a3feb5d95df04deae3afc8c8f6abc1
Nixtrox <nixtrox.vidi@gmail.com>
2024-03-19 10:35:29 +0100

    Basic Backend functions, get,post

--- JatekKonzolokBackEnd/index.js
+++ JatekKonzolokBackEnd/index.js
@@ -11,3 +11,37 @@ const jsonParser = new bodyParser.json();
 
 
 app.use(cors());
+
+
+const db = mysql.createPool(
+    {
+        host: "localhost",
+        user: 'root',
+        password: '',
+        database: 'JatekKonzolok'
+    }
+).promise();
+
+
+
+app.get("/",(req,res) =>{
+    res.send("VargaDavid_JatekKonzolok")
+})
+
+
+app.get("/Konzolok",async(req,res)=>{
+    const temp = await db.query("SELECT * FROM Konzolok")
+    const rows = temp[0];
+    const fields = temp[1];
+    res.send(rows);
+})
+
+app.post("/Konzolok", jsonParser,async(req,res)=>{
+    let consoleData = [req.body.Name,req.body.Brand,req.body.Price]
+    const insert = db.query("INSERT INTO Konzolok(Name,Brand,Price) VALUES (?,?,?)",consoleData)
+    res.send("200");
+})
+
+
+
+app.listen(3000);
